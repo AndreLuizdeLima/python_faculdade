7b8794b5681882c886687dc1593fedb2ebcf2913
Andre <andreluizjp2015@gmail.com>
2024-04-10 21:49:16 -0300

    Commit automatico Powershell 04/10/2024 21:49:15

---
+++ aula3/ex1.py
@@ -0,0 +1,7 @@
+#estruturas de repetição while
+i = 10
+x = 1
+while i > 0:
+    print(x)
+    x+= 1
+    i-=1
---
+++ aula3/ex2.py
@@ -0,0 +1,9 @@
+print('Tabuada')
+
+num1 = int(input('Deseja saber a tabuada de qual numero: '))
+
+i = 10
+while i > 0:
+    valor = num1 * i
+    print(f'{num1} x {i} = {valor}')
+    i-=1
